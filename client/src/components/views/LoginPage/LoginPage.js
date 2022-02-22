f296960a6f9e83f599da52715fcdca01ed405153
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2022-02-22 23:29:41 +0900

    로그인 페이지 만들기2

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,46 +1,63 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import {useDispatch} from 'react-redux';
+import {loginUser} from '../../../_actions/user_action';
 
 
-function LoginPage() {
+function LoginPage(props) {
+  const dispatch = useDispatch();
 
-    const [Email, setEmail] = useState("")
-    const [Password, setPassword] = useState("")
+  const [Email, setEmail] = useState('');
+  const [Password, setPassword] = useState('');
 
-    const onEmailHandler = (event) => {
-        setEmail(event.currentTarget.value)
-    }
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
 
-    const onPasswordHandler = (event) => {
-        setPassword(event.currentTarget.value)
-    }
-    
-    const onSubmitHandler = (event) => {
-        event.preventDefault(); //버튼 누를 때 새로고침 안하게해준다 
-    }
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
 
-    return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: '100%',
-          height: '100%',
-        }}
+  const onSubmitHandler = (event) => {
+    event.preventDefault(); //버튼 누를 때 새로고침 안하게해준다
+
+    let body = {
+      email: Email,
+      password: Password,
+    };
+
+    dispatch(loginUser(body)).then((response) => {
+      if (response.payload.loginSuccess) {
+        props.history.push('/');
+      } else {
+        alert('Error');
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <form
+        style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={onSubmitHandler}
       >
-        <form style={{ display: 'flex', flexDirection: 'column' }}
-            onSubmit={onSubmitHandler}
-        >
-          <label>Email</label>
-          <input type="email" value={Email} onChange={onEmailHandler} />
-          <label>Password</label>
-          <input type="password" value={Password} onChange={onPasswordHandler} />
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
 
-          <br />
-          <button type="submit">login</button>
-        </form>
-      </div>
-    );
+        <br />
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage
